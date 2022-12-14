7d207160c27b16a752af8b5033aff4b605b49d04
Michael Bennett <mvbennett9@gmail.com>
2022-12-14 17:23:08 -0500

    added geometry to turn into floppy disk later

--- main.js
+++ main.js
@@ -31,13 +31,14 @@ renderer.render( scene, camera );
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(40,40,40);
+pointLight.position.set(20,20,20);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // helpers
 // const lightHelper = new THREE.PointLightHelper(pointLight);
+// scene.add(lightHelper);
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper);
 
@@ -49,7 +50,7 @@ const addStar = () => {
   const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
   const star = new THREE.Mesh( geometry, material );
 
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 200 ) );
 
   star.position.set(x, y, z);
   scene.add(star);
@@ -76,11 +77,20 @@ mike.position.x = 2;
 
 // Icosahedron
 
-const icoGeometry = new THREE.TetrahedronGeometry(7, 2);
-const icoMaterial = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
-const icosahedron = new THREE.Mesh(icoGeometry, icoMaterial);
+// const icoGeometry = new THREE.TetrahedronGeometry(4, 2);
+// const icoMaterial = new THREE.MeshDepthMaterial( { color: 0xFF6347 } );
+// const icosahedron = new THREE.Mesh(icoGeometry, icoMaterial);
 
-scene.add(icosahedron);
+// scene.add(icosahedron);
+
+// floppydisk?
+const diskGeometry = new THREE.BoxGeometry( 3, 3, .1 );
+const diskMaterial = new THREE.MeshNormalMaterial( {color: 0x0000a6} );
+const disk = new THREE.Mesh( diskGeometry, diskMaterial );
+scene.add( disk );
+
+disk.position.z = 20;
+disk.position.x = 2;
 
 // moving camera on scroll
 const moveCamera = () => {
@@ -88,6 +98,9 @@ const moveCamera = () => {
   console.log(t);
   mike.rotation.y += 0.01;
   mike.rotation.z += 0.01;
+
+  disk.rotation.y += 0.01;
+  disk.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
